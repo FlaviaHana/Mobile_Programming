267949c86e116bfde839d40226490a6bfd74b900
flavia hana <flaviahana43@gmail.com>
2025-10-30 14:53:52 +0700

    Tugas Praktikum 7

---
+++ pertemuan_7/lib/add_note_screen.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import '../db/database_helper.dart';
+import '../models/note.dart';
+
+class AddNoteScreen extends StatefulWidget {
+  final Note? note; 
+
+  const AddNoteScreen({super.key, this.note});
+
+  @override
+  State<AddNoteScreen> createState() => _AddNoteScreenState();
+}
+
+class _AddNoteScreenState extends State<AddNoteScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final titleController = TextEditingController();
+  final contentController = TextEditingController();
+  final dbHelper = DatabaseHelper();
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.note != null) {
+      titleController.text = widget.note!.title;
+      contentController.text = widget.note!.content;
+    }
+  }
+
+  void _saveNote() async {
+    if (_formKey.currentState!.validate()) {
+      if (widget.note == null) {
+        // Tambah catatan baru
+        final newNote = Note(
+          title: titleController.text,
+          content: contentController.text,
+          createdAt: DateTime.now(),
+        );
+        await dbHelper.insertNote(newNote);
+      } else {
+        final updatedNote = Note(
+          id: widget.note!.id,
+          title: titleController.text,
+          content: contentController.text,
+          createdAt: widget.note!.createdAt,
+        );
+        await dbHelper.updateNote(updatedNote);
+      }
+      Navigator.pop(context, true); 
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.note == null ? "Tambah Catatan" : "Edit Catatan"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                controller: titleController,
+                decoration: const InputDecoration(labelText: 'Judul'),
+                validator: (value) =>
+                    value!.isEmpty ? 'Judul tidak boleh kosong' : null,
+              ),
+              const SizedBox(height: 12),
+              TextFormField(
+                controller: contentController,
+                decoration: const InputDecoration(labelText: 'Isi catatan'),
+                maxLines: 5,
+                validator: (value) =>
+                    value!.isEmpty ? 'Isi tidak boleh kosong' : null,
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: _saveNote,
+                child: Text(widget.note == null ? 'Simpan' : 'Perbarui'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
